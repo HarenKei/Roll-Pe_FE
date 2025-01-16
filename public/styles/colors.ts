54975dcb7147977522ad37b09df7f164db27716d
HarenKei <seungmin1266@gmail.com>
2025-01-17 01:06:17 +0900

    Style: Add type to each const

--- public/styles/colors.ts
+++ public/styles/colors.ts
@@ -1,9 +1,9 @@
-export const ROLLPE_MAIN = "#FF5656";
+export const ROLLPE_MAIN: string = "#FF5656";
 
-export const ROLLPE_PRIMARY = "#FFFFFF";
+export const ROLLPE_PRIMARY: string = "#FFFFFF";
 
-export const ROLLPE_SECONDARY = "#181818";
+export const ROLLPE_SECONDARY: string = "#181818";
 
-export const ROLLPE_GRAY = "#AAAAAA";
+export const ROLLPE_GRAY: string = "#AAAAAA";
 
-export const ROLLPE_SECTION_BACKGROUND = "#F4F4F4";
+export const ROLLPE_SECTION_BACKGROUND: string = "#F4F4F4";
