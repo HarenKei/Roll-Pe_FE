2dee911ca1a941955f4bd99af6f85476f5147518
HarenKei <seungmin1266@gmail.com>
2025-01-17 01:04:55 +0900

    Styles: Add color libraries

---
+++ public/styles/colors.ts
@@ -0,0 +1,9 @@
+export const ROLLPE_MAIN = "#FF5656";
+
+export const ROLLPE_PRIMARY = "#FFFFFF";
+
+export const ROLLPE_SECONDARY = "#181818";
+
+export const ROLLPE_GRAY = "#AAAAAA";
+
+export const ROLLPE_SECTION_BACKGROUND = "#F4F4F4";
