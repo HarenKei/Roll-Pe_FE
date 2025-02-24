db5c7dbe97938f94ff3d3ad42b80b2ef1aab9559
HarenKei <seungmin1266@gmail.com>
2025-02-24 16:16:18 +0900

    Feat: Add axios request interceptor

--- public/axios/axiosInstance.ts
+++ public/axios/axiosInstance.ts
@@ -2,17 +2,25 @@ import axios from "axios";
 import { cookies } from "next/headers";
 import { getToken } from "../utils/getCookies";
 
+// credentials가 필요 없는 요청
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+})
 
+// credentials가 필요한 요청
 export const axiosInstanceAuth = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 })
 
 axiosInstanceAuth.interceptors.request.use(
   async (config) => {
     const accessToken = cookies().get("accessToken");
+    console.log("인터셉터!!");
+    console.log(accessToken);
 
     if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
+      config.headers.Authorization = `Bearer ${accessToken.value}`;
     }
 
     return config;
