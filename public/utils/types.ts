e37dfafa8679ba202f12ee90bb3c6a8f56c19722
HarenKei <seungmin1266@gmail.com>
2025-02-26 15:05:04 +0900

    Feat: add handler for refresh token response errors

--- public/utils/types.ts
+++ public/utils/types.ts
@@ -1,3 +1,5 @@
+import { StaticGenerationAsyncStorage } from "next/dist/client/components/static-generation-async-storage.external";
+
 //! Rollpe List 관련
 export interface RollpeListItemProps {
   rollpeId: string;
@@ -29,4 +31,34 @@ export interface MainRollpeCardProps {
 export interface userIntroResponse {
   host: number;
   heart: number;
+}
+
+// ! User 관련
+export interface User {
+  id: string; //uuid
+  name: string;
+  email: string;
+  register: "Apple" | "Google" | "Kakao" | "Email";
+}
+
+//! Rollpe 관련
+export interface Rollpe {
+  id: string;
+  title: string;
+  host: User;
+  startDate: string;
+  endDate: string;
+  isPublic: boolean;
+  writers: User[];
+  theme: string;
+  size: string;
+}
+
+//! Heart 관련
+export interface Heart {
+  id: string;
+  index: number;
+  writer: User;
+  content: string;
+  creratedAt: string;
 }
