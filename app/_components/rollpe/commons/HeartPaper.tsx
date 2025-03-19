856d9544bff5020a46cf09da36cc5933bbe5661e
HarenKei <seungmin1266@gmail.com>
2025-03-19 16:36:28 +0900

    Feat: Implement Test Rollpe

--- app/_components/rollpe/commons/HeartPaper.tsx
+++ app/_components/rollpe/commons/HeartPaper.tsx
@@ -1,8 +1,87 @@
 "use client";
 import styled from "styled-components";
+import { Heart } from "@/public/utils/types";
+import { COLORS } from "@/public/styles/colors";
 
-const HeartPaper: React.FC = () => {
+interface HeartPaperProps {
+  color: string;
+  deg: number;
+  margin: React.CSSProperties["margin"];
+}
+
+export const HeartPaper: React.FC = () => {
   return <></>;
 };
 
-export default HeartPaper;
+export const HeartPaperPreview: React.FC<HeartPaperProps> = ({
+  color,
+  deg,
+  margin,
+}) => {
+  return (
+    <HeartPaperPreviewWrapper>
+      <HeartPaperPreviewContainer
+        color={color}
+        deg={deg}
+        style={{ margin: margin }}
+      >
+        <ContentsWrapper>
+          <p>김테스트야 생일 축하해, 우리 곁에 와줘서 고마워!!!!</p>
+        </ContentsWrapper>
+      </HeartPaperPreviewContainer>
+    </HeartPaperPreviewWrapper>
+  );
+};
+
+const HeartPaperPreviewWrapper = styled.button`
+  all: unset;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 90%;
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: translateY(-10px);
+  }
+`;
+
+const HeartPaperPreviewContainer = styled.div<{
+  color: string;
+  deg: number;
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 90%;
+  height: 90%;
+  min-width: 3.5em;
+  min-height: 4em;
+  max-height: 10em;
+
+  transform: rotate(${(props) => props.deg}deg);
+  background: ${(props) => props.color};
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  font-family: var(--font-hakgyoanshim);
+`;
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1em;
+  width: calc(100% - 2em);
+  height: calc(100% - 2em);
+
+  & > p {
+    font-family: var(--font-nanumpen);
+    font-size: 1.25em;
+    color: ${COLORS.ROLLPE_SECONDARY};
+    line-height: auto;
+    text-align: center;
+    word-break: keep-all;
+  }
+`;
--- app/_components/rollpe/white/WhiteRollpe.tsx
+++ app/_components/rollpe/white/WhiteRollpe.tsx
@@ -1,11 +1,155 @@
 "use client";
 import styled from "styled-components";
-import HeartPaper from "../commons/HeartPaper";
+import { HeartPaperPreview } from "../commons/HeartPaper";
+import { Rollpe } from "@/public/utils/types";
 
 export const WhiteRollpe: React.FC = () => {
   return <></>;
 };
 
 export const WhiteRollpePriview: React.FC = () => {
-  return <></>;
+  return (
+    <PriviewWrapper>
+      <PreviewContainer>
+        <FirstRow>
+          <HeartPaperPreview
+            color={"#F228D3"}
+            deg={-2.72}
+            margin={"0rem 0rem 0rem 0rem"}
+          />
+          <HeartPaperPreview
+            color={"#F2EB28"}
+            deg={0}
+            margin={"0rem 0rem 0rem 0rem"}
+          />
+          <HeartPaperPreview
+            color={"#F2EB28"}
+            deg={5.39}
+            margin={"0rem 0rem 0rem 0rem"}
+          />
+          <HeartPaperPreview
+            color={"#28E8F2"}
+            deg={0.22}
+            margin={"0rem 0rem 0rem 0rem"}
+          />
+          <HeartPaperPreview
+            color={"#F2EB28"}
+            deg={-1.6}
+            margin={"0em 0em 0.3em 0em"}
+          />
+        </FirstRow>
+        <SecondRow>
+          <HeartPaperPreview
+            color={"#28E8F2"}
+            deg={2.78}
+            margin={"0em 0em 0.6em 0em"}
+          />
+          <div className={"title-wrapper"}>
+            <p>김테스트의 생일을 축하합니다!</p>
+          </div>
+          <HeartPaperPreview
+            color={"#F228D3"}
+            deg={3.1}
+            margin={"0em 0em 0.98em 0em"}
+          />
+        </SecondRow>
+        <ThirdRow>
+          <HeartPaperPreview
+            color={"#F2EB28"}
+            deg={-1.98}
+            margin={"0rem 0rem 0rem 0rem"}
+          />
+          <HeartPaperPreview
+            color={"#28E8F2"}
+            deg={4.03}
+            margin={"0.3em 0em 0em 0em"}
+          />
+          <HeartPaperPreview
+            color={"#F2EB28"}
+            deg={-1.98}
+            margin={"0em 0em 0.6em 0em"}
+          />
+          <HeartPaperPreview
+            color={"#F2EB28"}
+            deg={-0.5}
+            margin={"0rem 0rem 0rem 0rem"}
+          />
+          <HeartPaperPreview
+            color={"#28E8F2"}
+            deg={-2.83}
+            margin={"0em 0em 0em 0.28em"}
+          />
+        </ThirdRow>
+      </PreviewContainer>
+    </PriviewWrapper>
+  );
 };
+
+const PriviewWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  aspect-ratio: 297 / 210;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+
+const PreviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem;
+  width: calc(100% - 1rem);
+  height: calc(100% - 1rem);
+`;
+
+const FirstRow = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  width: 100%;
+  height: 100%;
+  min-height: 4em;
+  max-height: 10em;
+`;
+
+const SecondRow = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+
+  width: 100%;
+  height: 100%;
+  min-height: 4em;
+  max-height: 10em;
+
+  & > .title-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    padding: 0.25rem;
+    width: calc(100% - 0.5rem);
+    height: calc(100% - 0.5rem);
+
+    & > p {
+      text-align: center;
+      font-size: 1.35rem;
+      font-family: var(--font-jalnan);
+      line-height: auto;
+      word-break: keep-all;
+
+      @media (min-width: 768px) {
+        font-size: 3.25rem;
+      }
+    }
+  }
+`;
+
+const ThirdRow = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  width: 100%;
+  height: 100%;
+  min-height: 4em;
+  max-height: 10em;
+`;
