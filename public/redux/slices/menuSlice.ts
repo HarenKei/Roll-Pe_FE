200943a47d7d371c7642d341484e762021b614c0
HarenKei <seungmin1266@gmail.com>
2025-06-22 16:02:26 +0900

    Feat: Add slide menu slice

---
+++ public/redux/slices/menuSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState: boolean = false;
+
+const menuSlice = createSlice({
+  name: "menuSlice",
+  initialState: initialState,
+  reducers: {
+    openMenu: (state, action) => {
+      state = action.payload;
+    },
+    closeMenu: (state) => {
+      state = initialState;
+    }
+  },
+})
+
+
+
+export const { openMenu, closeMenu } = menuSlice.actions;
